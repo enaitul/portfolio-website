31abe164f78b02ec6c10faf38706e4355b20236d
Md Enaitul Hoque <127648815+enaitul@users.noreply.github.com>
2025-08-10 00:15:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for (tablink of tablinks){
+function opentab(tabname) {
+    for (tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for (tabcontent of tabcontents){
+    for (tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
@@ -13,41 +13,71 @@ function opentab(tabname){
 }
 
 var sidemenu = document.getElementById("sidemenu");
-var sidemenu = document.getElementById("sidemenu");
 var menuIcon = document.getElementById("menu-icon");
 
-function openmenu(){
+function openmenu() {
     sidemenu.style.right = "0";
-    menuIcon.style.display = "none"; 
+    menuIcon.style.display = "none";
 }
 
-function closemenu(){
+function closemenu() {
     sidemenu.style.right = "-200px";
-    menuIcon.style.display = "block"; 
+    menuIcon.style.display = "block";
 }
 
-function showMoreMessage(){
-     const msgSpan = document.getElementById("more-msg");
-        msgSpan.innerText = "Stay tuned. More projects coming soon.";
-        msgSpan.style.display = "inline";
+function showMoreMessage() {
+    const msgSpan = document.getElementById("more-msg");
+    msgSpan.innerText = "Stay tuned. More projects coming soon.";
+    msgSpan.style.display = "inline";
 
-        setTimeout(() => {
-            msgSpan.style.display = "none";
-        }, 3000); 
+    setTimeout(() => {
+        msgSpan.style.display = "none";
+    }, 3000);
 }
 
 function gitMessage() {
     alert("Check out my Github profile for more information.");
 }
 
+// Hover effect for right zone
 const header = document.getElementById("header");
 const rightZone = document.querySelector(".right-hover-zone");
 
-rightZone.addEventListener("mouseenter", () => {
-  header.classList.add("hover-right");
-});
+if (rightZone) {
+    rightZone.addEventListener("mouseenter", () => {
+        header.classList.add("hover-right");
+    });
 
-rightZone.addEventListener("mouseleave", () => {
-  header.classList.remove("hover-right");
+    rightZone.addEventListener("mouseleave", () => {
+        header.classList.remove("hover-right");
+    });
+}
+
+// Mobile tap effect
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth <= 768 && header) {
+        let toggled = false;
+        const iamText = document.getElementById("iam-text");
+        const originalBg = "images/background-phone.jpg";  // original
+        const newBg = "images/download.jpeg";              // new
+
+        // Set initial "I'm" color to white
+        if (iamText) {
+            iamText.style.color = "white";
+        }
+
+        header.addEventListener("click", function () {
+            header.style.backgroundImage = toggled
+                ? `url('${originalBg}')`
+                : `url('${newBg}')`;
+
+            // Toggle "I'm" color between white and black
+            if (iamText) {
+                iamText.style.color = toggled ? "white" : "black";
+            }
+
+            toggled = !toggled;
+        });
+    }
 });
 
